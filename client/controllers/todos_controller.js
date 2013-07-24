11d1161c7a6011e262084a07c9b5759bb6e3cfbd
Kyle Finley <git@kylefinley.com>
2013-07-24 15:07:57 -0500

    file structure: begin
    Creating the Global window.Todos was move to client/app/app.js to insure that it is loaded before the other files. Meteor loads the deepest files first.

---
+++ client/controllers/todos_controller.js
@@ -0,0 +1,54 @@
+
+Todos.TodosController = Ember.ArrayController.extend({
+  createTodo: function () {
+    // Get the todo title set by the "New Todo" text field
+    var title = this.get('newTitle');
+    if (!title.trim()) { return; }
+
+    // Create the new Todo model
+    var todo = Todos.Todo.createRecord({
+      title: title,
+      isCompleted: false
+    });
+
+    // Clear the "New Todo" text field
+    this.set('newTitle', '');
+
+    // Save the new model
+    todo.save();
+  },
+
+  remaining: function () {
+    return this.filterProperty('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function () {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterProperty('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  clearCompleted: function () {
+    var completed = this.filterProperty('isCompleted', true);
+    completed.invoke('deleteRecord');
+
+    this.get('store').save();
+  },
+
+  allAreDone: function (key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.get('store').save();
+      return value;
+    }
+  }.property('@each.isCompleted')
+});
