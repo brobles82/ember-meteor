6658035a1bbe075b36d54d193e959b54560e2206
Kyle Finley <git@kylefinley.com>
2013-07-24 13:54:02 -0500

    Todos: updated to use MeteorAdapter
    Removed FIXTURES, use Meteor.startup() to add fixtures instead. Maybe this should be added back at some point.

--- client/app.js
+++ client/app.js
@@ -44,31 +44,13 @@ Todos.TodosCompletedRoute = Ember.Route.extend({
 
 Todos.Store = DS.Store.extend({
   revision: 12,
-  adapter: DS.FixtureAdapter.create()
+  adapter: DS.MeteorAdapter.create()
 });
 
 Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
-
-Todos.Todo.FIXTURES = [
-  {
-    id: 1,
-    title: 'Learn Ember.js',
-    isCompleted: true
-  },
-  {
-    id: 2,
-    title: '...',
-    isCompleted: false
-  },
-  {
-    id: 3,
-    title: 'Profit!',
-    isCompleted: false
-  }
-];
 
 Todos.TodoController = Ember.ObjectController.extend({
   isEditing: false,
