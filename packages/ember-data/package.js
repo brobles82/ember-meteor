447e21f2c07f296755a0e9727d4aac65d3e9cfbb
Kyle Finley <git@kylefinley.com>
2013-09-02 13:07:06 -0500

    ember-data updated

--- packages/ember-data/package.js
+++ packages/ember-data/package.js
@@ -5,10 +5,9 @@ Package.describe({
 Package.on_use(function (api, where) {
   api.use('random');
 
-  api.add_files('ember-data.js', 'client');
-  api.add_files('meteor_adapter.js', 'client');
-
-  api.exportSymbol('DS', where);
+  api.add_files('bower_components/ember-data/ember-data.js', 'client', { bare: true });
+  // api.add_files('meteor_adapter.js', 'client');
+  api.export('ds');
 });
 
 Package.on_test(function (api) {
